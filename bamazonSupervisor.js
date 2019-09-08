509857e937697c1bc0ae102a959eeaf763294917
Abdullah Bayindir <byndrabdullah@gmail.com>
2019-09-08 16:30:30 -0500

    done with supervisor part. Read me left

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -28,51 +28,53 @@ inquirer.prompt([
             break;
         case "createDepartment":
             createDepartment()
-            break;   
+            break;
     }
 });
 
 
-function viewProductsByDepartment(){
+function viewProductsByDepartment() {
     connection.query(
-        "SELECT departments.department_id, departments.departments_name, departments.over_head_cost, products.product_sales, products.product_sales - departments.over_head_cost AS total_profit FROM departments RIGHT JOIN products ON departments.departments_name = products.department_name",
+        "SELECT departments.department_id, departments.departments_name, departments.over_head_cost, SUM(products.product_sales) AS product_sales, SUM(products.product_sales) - departments.over_head_cost AS total_profit FROM departments RIGHT JOIN products ON departments.departments_name = products.department_name GROUP BY departments_name",
         function (err, results) {
             if (err) {
                 throw err;
             }
+
             console.table(results)
+
         }
     )
 }
 
-function createDepartment(){
+function createDepartment() {
     inquirer.prompt([
         {
             type: "input",
             name: "name",
             message: "Enter the name of the new department.",
         }
-        ,{
+        , {
             type: "input",
             name: "overHeadCost",
             message: "What is the over head cost o the new department?",
         }
-    ]).then(function(response){
+    ]).then(function (response) {
         connection.query(
             "INSERT INTO `bamazon_db`.`departments` (`departments_name`, `over_head_cost`) VALUES (?, ?)",
             [response.name, response.overHeadCost],
-            function(err, result){
+            function (err, result) {
                 if (err) {
                     throw err;
                 }
                 console.log("Added!")
                 viewDepartmentsTable();
-               }
+            }
         )
     })
 }
 
-function viewDepartmentsTable(){
+function viewDepartmentsTable() {
     connection.query(
         "SELECT * FROM departments",
         function (err, results) {
